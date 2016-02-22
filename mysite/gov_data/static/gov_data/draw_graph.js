6f8c254e347eb4a539baea612f2bae03b9e1aaad
Michael Vasiliou <mvasiliou94@gmail.com>
2016-02-21 22:05:04 -0600

    Updating for CSIL

--- mysite/gov_data/static/gov_data/draw_graph.js
+++ mysite/gov_data/static/gov_data/draw_graph.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
       .range(['#FF563B','#29859F','#FF9E3B']);
   
   var force = d3.layout.force()
-      .charge(-300)
+      .charge(-500)
       .linkDistance(function(d){ return (d.weight) + (Math.log((1 / d.weight)) * -50); })
       .size([width, height]);
   
@@ -23,7 +23,7 @@ $(document).ready(function(){
       .style('fill', 'none')
       .style('stroke', '#000')
       .style('stroke-width', 10);
-  
+
   function add_nodes_and_edges(file_name) {
       svg.selectAll('.node')
           .remove()
@@ -37,30 +37,64 @@ $(document).ready(function(){
           .nodes(graph.nodes)
           .links(graph.links)
           .start();
-    
+
       var link = svg.selectAll(".link")
           .data(graph.links)
           .enter().append("line")
           .attr("class", "link")
-          .style("stroke-width", function(d) { return d.weight / 2; });
-    
+          .style("stroke-width", function(d) { return d.weight / 5; })
+          .style("stroke", '#C2C2C2');
+      
       link.append("title")
           .text(function(d) {return d.weight})     
       var node = svg.selectAll(".node")
           .data(graph.nodes)
           .enter().append("circle")
           .attr("class", "node")
+          .attr("r", function(d){ return (d.size / 15) + 5})
+          .style("fill", function(d) { return color(d.party); })
+          .call(force.drag)
           .on('dblclick', function(d, i) {
             url = 'senators/' + d.id + '/';
             window.location.href = url;
           })
-          .attr("r", function(d){ return d.size / 15})
-          .style("fill", function(d) { return color(d.party); })
-          .call(force.drag);
-    
+          .on('mouseover', function(d) {
+              node.transition()
+                  .duration(300)
+                  .style("r", function(l) {
+                      if (d === l)
+                        return (d.size / 10) + 5;
+                      
+                      })
+
+              link.transition()
+                  .duration(300)
+                  .style('stroke', function(l) {
+                  if (d === l.source || d === l.target)
+                    return color(d.party);
+                  else
+                    return '#C2C2C2';
+                  })
+                  .style('stroke-width', function(l) {
+                  if (d === l.source || d === l.target)
+                    return l.weight;
+                  else
+                    return l.weight / 10;
+                  });
+          })
+          .on('mouseout', function(d) {
+              node.transition()
+                .duration(300)
+                .style('r', function(l){ return (l.size / 15) + 5})
+              link.transition()
+                .duration(300)
+                .style('stroke', '#C2C2C2')
+                .style('stroke-width', function(l){
+                  return l.weight / 5;
+                });
+          });
       node.append("title")
-          .text(function(d) { return d.label; });
-    
+            .text(function(d) { return d.label; })
       force.on("tick", function() {
         node.attr("cx", function(d) { return d.x = Math.max(15, Math.min(width - 15, d.x)); })
             .attr("cy", function(d) { return d.y = Math.max(15, Math.min(height - 15, d.y)); });
